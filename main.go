e8452516e7af57f7513de9aa535403a21682e05c
monicahardt <monha@itu.dk>
2022-11-09 17:59:57 +0100

    maybe working. Last minut added lamportclock

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 	"strconv"
 	"time"
 
-	enter "github.com/rakulmaria/handin_04/grpc"
+	recieve "github.com/rakulmaria/handin_04/grpc"
 	"google.golang.org/grpc"
 )
 
@@ -27,7 +27,7 @@ func main() {
 	p := &peer{
 		id:               ownPort,
 		amountOfRequests: make(map[int32]int32),
-		clients:          make(map[int32]enter.EnterClient),
+		clients:          make(map[int32]recieve.RecieveClient),
 		ctx:              ctx,
 	}
 
@@ -37,7 +37,7 @@ func main() {
 		log.Fatalf("Failed to listen on port: %v", err)
 	}
 	grpcServer := grpc.NewServer()
-	enter.RegisterEnterServer(grpcServer, p)
+	recieve.RegisterRecieveServer(grpcServer, p)
 
 	go func() {
 		if err := grpcServer.Serve(list); err != nil {
@@ -59,63 +59,87 @@ func main() {
 			log.Fatalf("Could not connect: %s", err)
 		}
 		defer conn.Close()
-		c := enter.NewEnterClient(conn)
+		c := recieve.NewRecieveClient(conn)
 		p.clients[port] = c
 	}
 
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
-		p.SendRequestToAll()
+		p.Enter()
 	}
 }
 
 type peer struct {
-	enter.UnimplementedEnterServer
+	recieve.UnimplementedRecieveServer
 	id               int32
 	lamport          int32
 	isUsing          bool
 	amountOfRequests map[int32]int32
 	deferQueue       []int32
 	state            State
-	clients          map[int32]enter.EnterClient
+	clients          map[int32]recieve.RecieveClient
 	ctx              context.Context
-	channel          chan enter.Request
+	channel          chan recieve.Request
 }
 
-func Count() {
-	time.Sleep(5 * time.Second)
-	counter++
-}
-
-func (p *peer) Enter(ctx context.Context, req *enter.Request) (*enter.Reply, error) {
+func (p *peer) Recieve(ctx context.Context, req *recieve.Request) (*recieve.Reply, error) {
 	id := req.Id
+	p.lamport = req.Lamport;
+	p.lamport++;
+	fmt.Printf("peer with id: %v now has lamportclock: %v",id,p.lamport)
+	
 	p.amountOfRequests[id] += 1
-
 	// check if you sent a request yourself && check if you are in the critical section.
 	// in case you are requesting at the same time as the other, the one with the smallest lamport ts wins
 	// otherwise, you defer the request
-
 	if p.state == "held" || (p.state == "wanted" && (req.Lamport > p.lamport)) {
-		p.deferQueue = append(p.deferQueue, req.Id)
+		//p.deferQueue = append(p.deferQueue, req.Id)
+		fmt.Print("State held or wanted with smallest lamport")
+		time.Sleep(5 * time.Second)
+		reply := p.Exit()
+		fmt.Println("Released")
+		p.lamport++;
+		fmt.Printf("peer with id: %v now has lamportclock: %v",id,p.lamport)
+		return reply, nil
+	} else {
+		fmt.Printf("you take it person with id: %v", id)
+		p.lamport++;
+		fmt.Printf("peer with id: %v now has lamportclock: %v",id,p.lamport)
+		rep := &recieve.Reply{Id: p.amountOfRequests[id]}
+		return rep, nil
 	}
-
-	rep := &enter.Reply{Amount: p.amountOfRequests[id]}
-	return rep, nil
-
 }
 
-func (p *peer) SendRequestToAll() {
-	request := &enter.Request{Id: p.id, Lamport: p.lamport}
-	for id, client := range p.clients {
-		reply, err := client.Enter(p.ctx, request)
+func (p *peer) Enter() {
+	isAvailable := false
+	p.state = "wanted"
+	p.lamport++;
+	fmt.Printf("peer with id: %v now has lamportclock: %v",p.id,p.lamport)
+	request := &recieve.Request{Id: p.id, Lamport: p.lamport}
+	for _, client := range p.clients {
+		rep, err := client.Recieve(p.ctx, request)
+		p.lamport = rep.Lamport
+		p.lamport++;
+		fmt.Printf("peer with id: %v now has lamportclock: %v",p.id,p.lamport)
 		if err != nil {
 			fmt.Println("something went wrong")
 		}
-		fmt.Printf("Got reply from id %v: %v\n", id, reply.Amount)
-		go Count()
-
+	}
+	fmt.Printf("recieved message from everone. Person with id: %v now has the thing", p.id)
+	isAvailable = true
+	//recieved all replies
+	if(isAvailable){
+		p.state = "held"
 	}
 }
+
+func (p *peer) Exit()(*recieve.Reply){
+	fmt.Printf("peer with id: %v exited the thing", p.id)
+	p.state = "realeased"
+	rep := &recieve.Reply{Id: p.id, Lamport: p.lamport}
+	return rep
+}
+
 
 // our enum for State
 type State string

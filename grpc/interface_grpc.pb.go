25e298f61a8cacc68132e4a4df89d41b93aec5a1
rakul <rakt@itu.dk>
2022-11-09 16:16:54 +0100

    step 0

--- grpc/interface_grpc.pb.go
+++ grpc/interface_grpc.pb.go
@@ -4,7 +4,7 @@
 // - protoc             v3.21.6
 // source: grpc/interface.proto
 
-package ping
+package handin_04
 
 import (
 	context "context"
@@ -18,86 +18,86 @@ import (
 // Requires gRPC-Go v1.32.0 or later.
 const _ = grpc.SupportPackageIsVersion7
 
-// PingClient is the client API for Ping service.
+// EnterClient is the client API for Enter service.
 //
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
-type PingClient interface {
-	Ping(ctx context.Context, in *Request, opts ...grpc.CallOption) (*Reply, error)
+type EnterClient interface {
+	Enter(ctx context.Context, in *Request, opts ...grpc.CallOption) (*Reply, error)
 }
 
-type pingClient struct {
+type enterClient struct {
 	cc grpc.ClientConnInterface
 }
 
-func NewPingClient(cc grpc.ClientConnInterface) PingClient {
-	return &pingClient{cc}
+func NewEnterClient(cc grpc.ClientConnInterface) EnterClient {
+	return &enterClient{cc}
 }
 
-func (c *pingClient) Ping(ctx context.Context, in *Request, opts ...grpc.CallOption) (*Reply, error) {
+func (c *enterClient) Enter(ctx context.Context, in *Request, opts ...grpc.CallOption) (*Reply, error) {
 	out := new(Reply)
-	err := c.cc.Invoke(ctx, "/ping.ping/ping", in, out, opts...)
+	err := c.cc.Invoke(ctx, "/handin_04.enter/enter", in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
 	return out, nil
 }
 
-// PingServer is the server API for Ping service.
-// All implementations must embed UnimplementedPingServer
+// EnterServer is the server API for Enter service.
+// All implementations must embed UnimplementedEnterServer
 // for forward compatibility
-type PingServer interface {
-	Ping(context.Context, *Request) (*Reply, error)
-	mustEmbedUnimplementedPingServer()
+type EnterServer interface {
+	Enter(context.Context, *Request) (*Reply, error)
+	mustEmbedUnimplementedEnterServer()
 }
 
-// UnimplementedPingServer must be embedded to have forward compatible implementations.
-type UnimplementedPingServer struct {
+// UnimplementedEnterServer must be embedded to have forward compatible implementations.
+type UnimplementedEnterServer struct {
 }
 
-func (UnimplementedPingServer) Ping(context.Context, *Request) (*Reply, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method Ping not implemented")
+func (UnimplementedEnterServer) Enter(context.Context, *Request) (*Reply, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method Enter not implemented")
 }
-func (UnimplementedPingServer) mustEmbedUnimplementedPingServer() {}
+func (UnimplementedEnterServer) mustEmbedUnimplementedEnterServer() {}
 
-// UnsafePingServer may be embedded to opt out of forward compatibility for this service.
-// Use of this interface is not recommended, as added methods to PingServer will
+// UnsafeEnterServer may be embedded to opt out of forward compatibility for this service.
+// Use of this interface is not recommended, as added methods to EnterServer will
 // result in compilation errors.
-type UnsafePingServer interface {
-	mustEmbedUnimplementedPingServer()
+type UnsafeEnterServer interface {
+	mustEmbedUnimplementedEnterServer()
 }
 
-func RegisterPingServer(s grpc.ServiceRegistrar, srv PingServer) {
-	s.RegisterService(&Ping_ServiceDesc, srv)
+func RegisterEnterServer(s grpc.ServiceRegistrar, srv EnterServer) {
+	s.RegisterService(&Enter_ServiceDesc, srv)
 }
 
-func _Ping_Ping_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+func _Enter_Enter_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
 	in := new(Request)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
 	if interceptor == nil {
-		return srv.(PingServer).Ping(ctx, in)
+		return srv.(EnterServer).Enter(ctx, in)
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: "/ping.ping/ping",
+		FullMethod: "/handin_04.enter/enter",
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(PingServer).Ping(ctx, req.(*Request))
+		return srv.(EnterServer).Enter(ctx, req.(*Request))
 	}
 	return interceptor(ctx, in, info, handler)
 }
 
-// Ping_ServiceDesc is the grpc.ServiceDesc for Ping service.
+// Enter_ServiceDesc is the grpc.ServiceDesc for Enter service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
-var Ping_ServiceDesc = grpc.ServiceDesc{
-	ServiceName: "ping.ping",
-	HandlerType: (*PingServer)(nil),
+var Enter_ServiceDesc = grpc.ServiceDesc{
+	ServiceName: "handin_04.enter",
+	HandlerType: (*EnterServer)(nil),
 	Methods: []grpc.MethodDesc{
 		{
-			MethodName: "ping",
-			Handler:    _Ping_Ping_Handler,
+			MethodName: "enter",
+			Handler:    _Enter_Enter_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
--- main.go
+++ main.go
@@ -8,10 +8,14 @@ import (
 	"net"
 	"os"
 	"strconv"
+	"time"
 
-	ping "github.com/rakulmaria/handin_04/grpc"
+	enter "github.com/rakulmaria/handin_04/grpc"
 	"google.golang.org/grpc"
 )
+
+// our critical section is a counter. starts at 0
+var counter = 0
 
 func main() {
 	arg1, _ := strconv.ParseInt(os.Args[1], 10, 32)
@@ -21,10 +25,10 @@ func main() {
 	defer cancel()
 
 	p := &peer{
-		id:            ownPort,
-		amountOfPings: make(map[int32]int32),
-		clients:       make(map[int32]ping.PingClient),
-		ctx:           ctx,
+		id:               ownPort,
+		amountOfRequests: make(map[int32]int32),
+		clients:          make(map[int32]enter.EnterClient),
+		ctx:              ctx,
 	}
 
 	// Create listener tcp on port ownPort
@@ -33,7 +37,7 @@ func main() {
 		log.Fatalf("Failed to listen on port: %v", err)
 	}
 	grpcServer := grpc.NewServer()
-	ping.RegisterPingServer(grpcServer, p)
+	enter.RegisterEnterServer(grpcServer, p)
 
 	go func() {
 		if err := grpcServer.Serve(list); err != nil {
@@ -55,39 +59,70 @@ func main() {
 			log.Fatalf("Could not connect: %s", err)
 		}
 		defer conn.Close()
-		c := ping.NewPingClient(conn)
+		c := enter.NewEnterClient(conn)
 		p.clients[port] = c
 	}
 
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
-		p.sendPingToAll()
+		p.SendRequestToAll()
 	}
 }
 
 type peer struct {
-	ping.UnimplementedPingServer
-	id            int32
-	amountOfPings map[int32]int32
-	clients       map[int32]ping.PingClient
-	ctx           context.Context
+	enter.UnimplementedEnterServer
+	id               int32
+	lamport          int32
+	isUsing          bool
+	amountOfRequests map[int32]int32
+	deferQueue       []int32
+	state            State
+	clients          map[int32]enter.EnterClient
+	ctx              context.Context
+	channel          chan enter.Request
 }
 
-func (p *peer) Ping(ctx context.Context, req *ping.Request) (*ping.Reply, error) {
+func Count() {
+	time.Sleep(5 * time.Second)
+	counter++
+}
+
+func (p *peer) Enter(ctx context.Context, req *enter.Request) (*enter.Reply, error) {
 	id := req.Id
-	p.amountOfPings[id] += 1
+	p.amountOfRequests[id] += 1
 
-	rep := &ping.Reply{Amount: p.amountOfPings[id]}
+	// check if you sent a request yourself && check if you are in the critical section.
+	// in case you are requesting at the same time as the other, the one with the smallest lamport ts wins
+	// otherwise, you defer the request
+
+	if p.state == "held" || (p.state == "wanted" && (req.Lamport > p.lamport)) {
+		p.deferQueue = append(p.deferQueue, req.Id)
+	}
+
+	rep := &enter.Reply{Amount: p.amountOfRequests[id]}
 	return rep, nil
+
 }
 
-func (p *peer) sendPingToAll() {
-	request := &ping.Request{Id: p.id}
+func (p *peer) SendRequestToAll() {
+	request := &enter.Request{Id: p.id, Lamport: p.lamport}
 	for id, client := range p.clients {
-		reply, err := client.Ping(p.ctx, request)
+		reply, err := client.Enter(p.ctx, request)
 		if err != nil {
 			fmt.Println("something went wrong")
 		}
 		fmt.Printf("Got reply from id %v: %v\n", id, reply.Amount)
+		go Count()
+
 	}
 }
+
+// our enum for State
+type State string
+
+const (
+	released       = "released"
+	held           = "held"
+	wanted         = "wanted"
+	undefinedState = "illegal"
+)
